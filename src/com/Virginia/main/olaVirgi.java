fba9fe47d1defaaee486c4ed51ececcd7ca53fbd
Christian Ontiveros <contiverosv@outlook.com>
2022-10-26 11:50:44 -0500

    vistos metodos

--- src/com/Virginia/main/olaVirgi.java
+++ src/com/Virginia/main/olaVirgi.java
@@ -3,9 +3,14 @@ package com.Virginia.main;
 public class olaVirgi {
 
 	public static void main(String[] args) {
-		
-		
-		
+		System.out.println("El resultado de la suma es " + suma(1,1));
+		int resultadoSuma = suma(5000,-4000);
+		int adicionUwu = suma(resultadoSuma, 10);
+		System.out.println(adicionUwu);
+	}
+	
+	static int suma(int numUno, int numDos) {
+		return numUno + numDos;
 	}
 	
 }
