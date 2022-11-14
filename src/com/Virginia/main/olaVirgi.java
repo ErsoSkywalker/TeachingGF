1b76bde9d782c8af3d27808ce55d268095773c65
Christian Ontiveros <contiverosv@outlook.com>
2022-11-13 19:35:04 -0600

    hello virgi hizo dos metodos

--- src/com/Virginia/main/olaVirgi.java
+++ src/com/Virginia/main/olaVirgi.java
@@ -7,10 +7,30 @@ public class olaVirgi {
 		int resultadoSuma = suma(5000,-4000);
 		int adicionUwu = suma(resultadoSuma, 10);
 		System.out.println(adicionUwu);
+		
+		System.out.println("hello_virgi acaba de crear una resta");
+		System.out.println("alo polisia " + resta(1,1));
+		System.out.println("Estoy multiplicando " + multiplicacion(1,1));
 	}
 	
 	static int suma(int numUno, int numDos) {
+		System.out.println("Estoy sumando");
 		return numUno + numDos;
+	}
+	
+	static int resta(int numUno, int numDos) {
+		return numUno - numDos;
+	}
+	
+	static int multiplicacion(int numUno, int numDos) {
+		return numUno * numDos;	
+	}
+	static String saludar() {
+		return "Hola";
+	}
+	
+	static void nada() {
+		System.out.println("Jola mundo");
 	}
 	
 }
